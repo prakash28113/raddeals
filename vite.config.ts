5aa4b9fac87dcfadff461056ad97516326f57574
Anirudh <103483232+Anirudh-98@users.noreply.github.com>
2025-05-24 20:27:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/raddeals", // Add the base path here
+  base: "/", // Add the base path here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
